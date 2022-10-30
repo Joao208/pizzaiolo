9657f200294ff160dc9d43c155c4c16bf405df40
joao208 <augustoj311@gmail.com>
2022-10-30 19:31:29 -0300

    feat: adding gideon font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,9 +39,13 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&family=Rufina:wght@400;700&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Gideon+Roman&family=Oxygen:wght@300;400;700&family=Rufina:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
