161f422d55ba4fce331533f88ba612454ad43aa3
joao208 <augustoj311@gmail.com>
2022-10-30 19:32:33 -0300

    feat: adding more styles

--- styles/index.ts
+++ styles/index.ts
@@ -1,4 +1,19 @@
 import styled, { css } from 'styled-components'
+import { CardStepDetailProps, TitleProps } from '../interfaces'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background: linear-gradient(
+      0deg,
+      rgba(103, 96, 0, 0.05),
+      rgba(103, 96, 0, 0.05)
+    ),
+    #fffbff;
+`
 
 export const Main = styled.div`
   background: #1d1c16;
@@ -9,11 +24,13 @@ export const Main = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  width: 100%;
 `
 
 export const ContainerText = styled.div``
 
-export const Title = styled.p`
+export const Title = styled.p<TitleProps>`
   font-family: 'Rufina';
   font-style: normal;
   font-weight: 400;
@@ -31,7 +48,7 @@ export const Title = styled.p`
   width: 744px;
   height: auto;
 
-  margin: 32px 0;
+  margin: 0;
 
   overflow: hidden;
 
@@ -39,6 +56,12 @@ export const Title = styled.p`
     color &&
     css`
       color: ${color};
+    `}
+
+  ${({ margin }) =>
+    margin &&
+    css`
+      margin: 39px 0;
     `}
 `
 
@@ -110,4 +133,200 @@ export const SlackInstallMain = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  gap: 96px;
+
+  padding: 96px 0;
+
+  width: 100%;
+`
+
+export const EndInstallMain = styled(SlackInstallMain)``
+
+export const StepNumberTitle = styled.p`
+  font-family: 'Rufina';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 28px;
+  line-height: 36px;
+  color: #1d1c16;
+
+  flex: none;
+
+  margin: 0;
+
+  span {
+    font-family: 'Gideon Roman';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 64px;
+    line-height: 83px;
+
+    text-align: right;
+
+    color: #676000;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
+`
+
+export const GithubInstallMain = styled.div`
+  background: linear-gradient(
+      0deg,
+      rgba(103, 96, 0, 0.11),
+      rgba(103, 96, 0, 0.11)
+    ),
+    #fffbff;
+
+  height: fit-content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 96px;
+
+  padding: 96px 0;
+
+  width: 100%;
+`
+
+export const CardStepDetail = styled.div<CardStepDetailProps>`
+  display: flex;
+
+  align-items: center;
+  justify-content: space-between;
+
+  width: 80%;
+
+  ${({ reverse }) =>
+    reverse &&
+    css`
+      flex-direction: row-reverse;
+    `}
+`
+
+export const StepDetailTitle = styled.p`
+  font-family: 'Rufina';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 28px;
+
+  color: #1d1c16;
+
+  flex: none;
+`
+
+export const StepDetailDescription = styled.p`
+  font-family: 'Oxygen';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+
+  letter-spacing: 0.4px;
+
+  color: #484740;
+
+  flex: none;
+
+  max-width: 540px;
+  overflow: hidden;
+
+  .code {
+    background: #e9e4be;
+    border-radius: 4px;
+
+    color: #1e1c05;
+
+    padding: 2px 4px;
+  }
+
+  .bold {
+    font-family: 'Oxygen';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 16px;
+
+    letter-spacing: 0.4px;
+
+    color: #484740;
+  }
+
+  a {
+  }
+`
+
+export const CodeBlock = styled.div`
+  background: #e9e4be;
+  border-radius: 4px;
+
+  padding: 16px;
+  margin-top: 6px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  p {
+    margin: 0;
+  }
+`
+
+export const Footer = styled.div`
+  background: linear-gradient(
+      0deg,
+      rgba(103, 96, 0, 0.05),
+      rgba(103, 96, 0, 0.05)
+    ),
+    #fffbff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 30px 0;
+  gap: 60px;
+
+  width: 100%;
+`
+
+export const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const FooterTitle = styled.p`
+  font-family: 'Rufina';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+
+  letter-spacing: 0.15px;
+
+  color: #32302b;
+`
+
+export const FooterText = styled.p`
+  font-family: 'Oxygen';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+
+  letter-spacing: 0.4px;
+
+  color: #605e57;
+  margin: 8px 0;
+`
+
+export const FooterLine = styled.hr`
+  width: 80%;
+  margin: 0;
 `
