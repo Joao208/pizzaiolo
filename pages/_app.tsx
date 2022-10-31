97fe6559480e758a6e9c85a235d91b8dd49d93f2
joao208 <augustoj311@gmail.com>
2022-10-30 23:12:37 -0300

    feat: adding scroll behavior

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+    scroll-behavior: smooth;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
@@ -20,6 +21,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
 
     overflow-x: hidden;
+    scroll-behavior: smooth;
   }
 
   * ::selection {
