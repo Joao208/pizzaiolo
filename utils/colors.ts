06f96882f6eab11c6e3379904a593cab84c01b16
joao208 <augustoj311@gmail.com>
2022-10-30 23:39:28 -0300

    feat: adding colors constants

---
+++ utils/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: '#ffffff',
+  primary: '#fffbff',
+  secondary: '#1d1c16',
+  third: '#d4ca51',
+  fourth: '#353100',
+  fifth: '#d9d9d9',
+  sixth: '#484740',
+  seventh: '#e9e4be',
+  eighth: '#676000',
+  ninth: '#1e1c05',
+  tenth: '#32302b',
+  eleventh: '#605e57',
+}
